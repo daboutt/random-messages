9c330b66008579cf164dd4af14ea5cfb957efaa5
baokun2115 <baotran2115@gmail.com>
2023-12-05 18:45:44 +0700

    Get the jokes from API

--- README.md
+++ README.md
@@ -1,7 +1,12 @@
 # Jokes generator
 
 ## Description
-Generate new message and predict your day
+Generate joke using Ninja keys
 
 ## How to use
-Run `node index.js` and check how is your day
+1. Log in/Sign up to https://api-ninjas.com/
+2. Get the API key in your account
+3. Change 'YOUR_API_KEY' to your key
+4. Change limit of jokes you want to get (from 1 to 30)
+5. Run `npm i` to install *dependencies*
+6. Run `node index.js` and get the jokes!
--- index.js
+++ index.js
@@ -1,57 +1,24 @@
-const generateRandomNumber = () => {
-  return Math.floor(Math.random() * 100);
+import axios from 'axios';
+
+const YOUR_API_KEY = '+Byofta/hCgUw69/SLtDhQ==XtpXOVgy8efMvlC4';
+
+const fetchJokes = async () => {
+  let limit = 3;
+  const response = await axios({
+    method: 'get',
+    url: `https://api.api-ninjas.com/v1/dadjokes?limit=${limit}`,
+    headers: { 'X-Api-Key': YOUR_API_KEY },
+    contentType: 'application/json',
+  });
+  return response;
 };
 
-var limit = 3;
-$.ajax({
-  method: 'GET',
-  url: 'https://api.api-ninjas.com/v1/jokes?limit=' + limit,
-  headers: { 'X-Api-Key': 'YOUR_API_KEY' },
-  contentType: 'application/json',
-  success: function (result) {
-    console.log(result);
-  },
-  error: function ajaxError(jqXHR) {
-    console.error('Error: ', jqXHR.responseText);
-  },
-});
-
-const shouldDoWhat = [
-  'clean your room',
-  'help your mom',
-  'call your ex',
-  'go to the gym',
-  'go to the beach',
-  'go to the movies',
-  'go to the park',
-];
-const howToDay = [
-  'not going well',
-  'going well',
-  'going great',
-  'going bad',
-  'going terrible',
-  'going awesome',
-  'going amazing',
-];
-
-const generateShouldDoWhatMessage = () => {
-  return shouldDoWhat[Math.floor(Math.random() * shouldDoWhat.length)];
-};
-
-const generateHowToDayMessage = () => {
-  return howToDay[Math.floor(Math.random() * howToDay.length)];
-};
-
-const messageGenerator = () => {
-  const shouldDoWhatMessage = generateShouldDoWhatMessage();
-  const howToDayMessage = generateHowToDayMessage();
-  const luckyNumber = generateRandomNumber();
-  return (
-    `Your day are ${howToDayMessage}` +
-    `\nYour lucky number is: ${luckyNumber}` +
-    `\nYou should ${shouldDoWhatMessage}`
-  );
-};
-
-console.log(messageGenerator());
+let data;
+fetchJokes()
+  .then((res) => {
+    data = res.data;
+    data.forEach((item) => console.log(item.joke));
+  })
+  .catch((err) => {
+    console.log(err);
+  });
